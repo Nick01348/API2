7606808298b117128cf18561f3f1bd9c774e7ba5
Nick01348 <nicolas-castanedas@unilibre.edu.co>
2020-05-26 18:03:35 -0500

    Creación de la tabla producto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :productos
   resources :usuarios
   resources :seccions
   resources :estados
---
+++ db/migrate/20200526230238_create_productos.rb
@@ -0,0 +1,12 @@
+class CreateProductos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :productos do |t|
+      t.string :nombreProducto
+      t.float :precio
+      t.text :descripcion
+      t.references :seccion, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
